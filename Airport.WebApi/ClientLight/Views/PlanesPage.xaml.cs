6eb67528b33f317beeed0158bb1a583742d5720f
Rostyslav Diakiv <rostyslav.diakiv@gmail.com>
2018-07-30 00:43:07 +0300

    +

---
+++ Airport.WebApi/ClientLight/Views/PlanesPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ClientLight.Views
+{
+    using ClientLight.ViewModel;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PlanesPage : Page
+    {
+        private PlanesViewModel ViewModel => DataContext as PlanesViewModel;
+
+        public PlanesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                await ViewModel.LoadDataAsync(WindowStates.CurrentState);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
